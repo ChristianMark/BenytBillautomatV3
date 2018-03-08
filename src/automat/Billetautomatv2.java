7d6db9ed02328cc5db21df4efbf9bbac07ad0f62
ChristianMark <christian.s.mark@hotmail.com>
2018-03-08 13:53:21 +0100

    Slet billetter og justering af BenytBilletautomatv2

--- src/automat/Billetautomatv2.java
+++ src/automat/Billetautomatv2.java
@@ -227,19 +227,19 @@ public class Billetautomatv2 {
                     switch (type){
                         case 0 : //voksen billet
                             antalBilletterSolgtType0 = antalBilletterSolgt;
-
+                            break;
                         case 1 ://ungdoms billet
                             antalBilletterSolgtType1 = antalBilletterSolgt;
-
+                            break;
                         case 2 : //barne billet
                             antalBilletterSolgtType2 = antalBilletterSolgt;
-
+                            break;
                         case 3 : //studenter billet
                             antalBilletterSolgtType3 = antalBilletterSolgt;
-
+                            break;
                         case 4 : //cykel billet
                             antalBilletterSolgtType4 = antalBilletterSolgt;
-
+                            break;
                         default:
                             break;
                     }
@@ -409,11 +409,26 @@ public class Billetautomatv2 {
             System.out.println("Der blev ikke fundet nogle log-elementer der skete før den angivede dato.");
         }
     }
-
+    
+    /**
+     * Køber en billet og tilføjer den til listen "Billetter".
+     */
     void koebBilletter(int valg_billet, int valg_zone) {
-        if(){
-            Billetter.add(new Billet(valg_billet, valg_zone));
-        }
-        
+
+        Billetter.add(new Billet(valg_billet, valg_zone));
+        return;
+    }
+
+    /**
+     * Sletter billetter fra listen "Billetter".
+     */
+    void sletBilletter(int valg_billet, int valg_zone, int valg_antal) {
+        int tæller = 1;
+        for (Billet element : Billetter){
+            if(valg_billet == element.getType() && valg_zone == element.getZoner() && tæller <= valg_antal){
+                Billetter.remove(element);
+                tæller++; // Holder styr på, hvor mange elementer der skal slettes
+            }
+        }        
     }
 }
