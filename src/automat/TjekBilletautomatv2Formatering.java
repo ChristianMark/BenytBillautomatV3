63b15d318a08e1e5542e7e563849806f66f85253
john <36444142+unizippro@users.noreply.github.com>
2018-03-09 12:54:44 +0100

    Testing formatting

---
+++ src/automat/TjekBilletautomatv2Formatering.java
@@ -0,0 +1,19 @@
+package automat;
+
+public class TjekBilletautomatv2Formatering
+{
+	public static void main(String[] arg)
+	{
+		Billetautomatv2 automat = new Billetautomatv2();
+
+		int antalFejl = 0;
+		int retur;
+		
+		
+		automat.indsaetPenge(100);
+                for (int i = 0; i < 5; i++){
+                    automat.koebBilletter(i, 5);
+                }
+                automat.endeligtKoeb();
+        }
+}
