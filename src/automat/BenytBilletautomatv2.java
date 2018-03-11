639f4e3987bfdfe19a4de6df83837a3ce00757f4
john <36444142+unizippro@users.noreply.github.com>
2018-03-11 13:12:33 +0100

    Switch for valg og oprydning

--- src/automat/BenytBilletautomatv2.java
+++ src/automat/BenytBilletautomatv2.java
@@ -32,16 +32,21 @@ public class BenytBilletautomatv2 {
                                 System.out.println("Tast 15 for at udskrive log med soegekriterier");
 				System.out.println("Tast 16 for at logge ud af montoertilstand");
 			}
+                        
                         int valg = tastatur.nextInt();
 			tastatur.nextLine();
                         
-			if (valg==1) {
-				System.out.print("Skriv beloeb: ");
-				int beloeb = tastatur.nextInt();
-				automat.indsaetPenge(beloeb);
-			}
-			else if (valg==2) {
-				//automat.udskrivBillet();
+                    switch (valg) {
+                        case 1://indput penge
+                            {
+                                System.out.print("Skriv beloeb: ");
+                                int beloeb = tastatur.nextInt();
+                                automat.indsaetPenge(beloeb);
+                                break;
+                            }
+                        case 2://Tilføj billetter til liste
+                            {
+                                //automat.udskrivBillet();
                                 System.out.println("Tast 0 for at købe en Voksen Billet til: "+Billet.getVoksenPris());
                                 System.out.println("Tast 1 for at købe en Ungdoms Billet til: "+Billet.getUngdomsPris());
                                 System.out.println("Tast 2 for at købe en Børne Billet til: "+Billet.getBarnePris());
@@ -54,14 +59,18 @@ public class BenytBilletautomatv2 {
                                 int valg_zone = tastatur.nextInt();
                                 tastatur.nextLine();
                                 automat.koebBilletter(valg_billet,valg_zone);
-			}
-			else if (valg==3) {
-				int beloeb = automat.returpenge();
-			}
-                        else if(valg==4){
-                                automat.endeligtKoeb();
-                        }
-                        else if(valg==6){
+                                break;
+                            }
+                        case 3://udskriv returpenge
+                            {
+                                automat.returpenge();
+                                break;
+                            }
+                        case 4: //endeligt køb
+                            automat.endeligtKoeb();
+                            break;
+                        case 6://slet biletter fra indkøbslisten
+                            {
                                 System.out.println("Tast 0 for at slette en Voksen Billet");
                                 System.out.println("Tast 1 for at slette en Ungdoms Billet");
                                 System.out.println("Tast 2 for at slette en Børne Billet");
@@ -76,94 +85,99 @@ public class BenytBilletautomatv2 {
                                 int valg_antal = tastatur.nextInt();
                                 tastatur.nextLine();
                                 automat.sletBilletter(valg_billet,valg_zone,valg_antal);
-                        }
-			else if (valg==10) {
-				System.out.print("Skriv kode: ");
-				String kode = tastatur.next();
-				automat.montoerLogin(kode);
-			}
-			else if (valg==11) {
-				System.out.println("Antal billetter solgt: " + automat.getAntalBilletterSolgt());
-				System.out.println("Total indkomst: " + automat.getTotal()+" kr");
-			}
-			else if (valg==12) {
-				automat.nulstil();
-			}
-			else if (valg==13) {
-                                System.out.println("Tast 0 for at ændre prisen på en Voksen Billet");
-                                System.out.println("Tast 1 for at ændre prisen på en Ungdoms Billet");
-                                System.out.println("Tast 2 for at ændre prisen på en Børne Billet");
-                                System.out.println("Tast 3 for at ændre prisen på en Studenter Billet");
-                                System.out.println("Tast 4 for at ændre prisen på en Cykel Billet");       
-                                int valg_billet = tastatur.nextInt();
+                                break;
+                            }
+                        case 10: //log ind som montør
+                            System.out.print("Skriv kode: ");
+                            String kode = tastatur.next();
+                            automat.montoerLogin(kode);
+                            break;
+                            
+                        case 11://get totalt antal billetter solgt
+                            System.out.println("Antal billetter solgt: " + automat.getAntalBilletterSolgt());
+                            System.out.println("Total indkomst: " + automat.getTotal()+" kr");
+                            break;
+                            
+                        case 12://nulstin maskine
+                            automat.nulstil();
+                            break;
+                            
+                        case 13://ændre prisen på billetter
+                            
+                            System.out.println("Tast 0 for at ændre prisen på en Voksen Billet");
+                            System.out.println("Tast 1 for at ændre prisen på en Ungdoms Billet");
+                            System.out.println("Tast 2 for at ændre prisen på en Børne Billet");
+                            System.out.println("Tast 3 for at ændre prisen på en Studenter Billet");
+                            System.out.println("Tast 4 for at ændre prisen på en Cykel Billet");       
+                            int valg_billet = tastatur.nextInt();
+                            tastatur.nextLine();
+                            System.out.print("Skriv den nye pris: ");
+                            int beloeb = tastatur.nextInt();
+                            Billet.setBilletPris(valg_billet, beloeb);
+                            break;
+                            
+                        case 14://udskriv hele loggen
+                            //automat.udskrivLog();
+                            automat.udskrivLog_event_liste(); // Bruger den nye log
+                            break;
+                            
+                        case 15: //søg i loggen
+                            System.out.println("\nTast et tal fra listen:");
+                            System.out.println("1: Opsaettelse af maskinen");
+                            System.out.println("2: Penge indsættelse");
+                            System.out.println("3: Udskrevne billetter");
+                            System.out.println("4: Penge returneret");
+                            System.out.println("5: Montoer logins");
+                            System.out.println("6: Aerndring af billetpriser");
+                            System.out.println("7: Aerndring i antal solgte billetter");
+                            System.out.println("8: Log udskrifter");
+                            System.out.println("9: Nulstilling af automaten");
+                            int valg2 = tastatur.nextInt();
+                            tastatur.nextLine();
+                            System.out.println("\nTast et tal fra listen:");
+                            // Man skal ikke kunne søge efter paremetre ved alle events.
+                            if(valg2 != 1 && valg2 != 3 && valg2 != 8 && valg2 != 9){
+                                System.out.println("0 for at soege på paremetre. ");
+                            }
+                            System.out.println("1 for at soege efter succesfulde handlinger.");
+                            System.out.println("2 for at soege efter fejlede handlinger.\n3 for at søge efter dato. \n4 for alle handlinger.");
+                            int valg3 = tastatur.nextInt();
+                            tastatur.nextLine();
+                            if(valg3 == 0 && (valg2 != 1 && valg2 != 3 && valg2 != 8 && valg2 != 9)){
+                                System.out.println("\nIndtast paremetre du vil soege over.");
+                                int valg4 = tastatur.nextInt();
                                 tastatur.nextLine();
-                                System.out.print("Skriv den nye pris: ");
-				int beloeb = tastatur.nextInt();
-				Billet.setBilletPris(valg_billet, beloeb);
-			}
-			else if (valg==14) {
-				//automat.udskrivLog();
-                                automat.udskrivLog_event_liste(); // Bruger den nye log
-			}
-                        else if (valg==15) {
-				System.out.println("\nTast et tal fra listen:");
-				System.out.println("1: Opsaettelse af maskinen");
-				System.out.println("2: Penge indsættelse");
-                                System.out.println("3: Udskrevne billetter");
-                                System.out.println("4: Penge returneret");
-                                System.out.println("5: Montoer logins");
-                                System.out.println("6: Aerndring af billetpriser");
-                                System.out.println("7: Aerndring i antal solgte billetter");
-                                System.out.println("8: Log udskrifter");
-                                System.out.println("9: Nulstilling af automaten");
-                                int valg2 = tastatur.nextInt();
-                                tastatur.nextLine();
-                                
-                                System.out.println("\nTast et tal fra listen:");
-                                // Man skal ikke kunne søge efter paremetre ved alle events.
-                                if(valg2 != 1 && valg2 != 3 && valg2 != 8 && valg2 != 9){
-                                    System.out.println("0 for at soege på paremetre. ");
-                                }
-                                    System.out.println("1 for at soege efter succesfulde handlinger.");
-                                    System.out.println("2 for at soege efter fejlede handlinger.\n3 for at søge efter dato. \n4 for alle handlinger.");
-                                    int valg3 = tastatur.nextInt();
-                                    tastatur.nextLine();
-                                
-                                
-                                if(valg3 == 0 && (valg2 != 1 && valg2 != 3 && valg2 != 8 && valg2 != 9)){
-                                    System.out.println("\nIndtast paremetre du vil soege over.");
+                                automat.find_trans_over(valg2, valg4);
+                            }else{
+                                switch(valg3){
+                                    case 1: automat.udskriver(valg2, 1);
+                                    break;
+                                    case 2: automat.udskriver(valg2, 0);
+                                    break;
+                                    case 3: System.out.println("\nTast et tal fra listen:");
+                                    System.out.println("0: for at søge indenfor den sidste time");
+                                    System.out.println("1: for at søge indenfor den sidste dag");
+                                    System.out.println("2: for at søge indenfor den sidste uge");
+                                    System.out.println("3: for at søge indenfor de sidste 30 dage");
                                     int valg4 = tastatur.nextInt();
                                     tastatur.nextLine();
-                                    automat.find_trans_over(valg2, valg4);
-                                }else{
-                                    switch(valg3){
-                                    case 1: automat.udskriver(valg2, 1);
-                                            break;
-                                    case 2: automat.udskriver(valg2, 0);
-                                            break;
-                                    case 3: System.out.println("\nTast et tal fra listen:");
-                                            System.out.println("0: for at søge indenfor den sidste time");
-                                            System.out.println("1: for at søge indenfor den sidste dag");
-                                            System.out.println("2: for at søge indenfor den sidste uge");
-                                            System.out.println("3: for at søge indenfor de sidste 30 dage");
-                                            int valg4 = tastatur.nextInt();
-                                            tastatur.nextLine();
-                                            System.out.println();
-                                            automat.datoudskriver(valg4,valg2);
-                                            break;
+                                    System.out.println();
+                                    automat.datoudskriver(valg4,valg2);
+                                    break;
                                     case 4: automat.udskriver(valg2, 2);
-                                            break;
+                                    break;
                                     default: System.out.println("Du valgte et ugyldigt svar: "+valg3);
-                                            break;
-                                    }                                    
+                                    break;                                    
                                 }
-			}                        
-                        else if (valg==16) {
-				automat.montoerLogin("");
-			}
-			else {
-				System.out.println("Ugyldigt valg, proev igen");
-			}
+                            }
+                            break;
+                        case 16:
+                            automat.montoerLogin("");
+                            break;
+                        default:
+                            System.out.println("Ugyldigt valg, proev igen");
+                            break;
+                    }
 		}
 	}
 }
